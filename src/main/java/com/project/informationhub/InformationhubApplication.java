f87e34d9a055a70aad303229cef4616c79f7d8fb
DKrtl <dogukan_kartal98@hotmail.com>
2021-02-16 19:24:26 +0000

    initial commit

---
+++ src/main/java/com/project/informationhub/InformationhubApplication.java
@@ -0,0 +1,13 @@
+package com.project.informationhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationhubApplication.class, args);
+	}
+
+}
