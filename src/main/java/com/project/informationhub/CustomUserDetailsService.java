95813642a71d77a6700259cc471299bd05ee4a5e
DKrtl <dogukan_kartal98@hotmail.com>
2021-02-19 20:32:54 +0000

    created custom user details class

--- src/main/java/com/project/informationhub/CustomUserDetailsService.java
+++ src/main/java/com/project/informationhub/CustomUserDetailsService.java
@@ -1,15 +1,23 @@
 package com.project.informationhub;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.finByUsername(username);
+        user.orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
+        return new CustomUserDetails(user.get());
     }
 }
