bcb55b69593a7e831f6bcc56d818eaf4641cc234
DKrtl <dogukan_kartal98@hotmail.com>
2021-02-19 19:18:20 +0000

    setup security configuration

---
+++ src/main/java/com/project/informationhub/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.project.informationhub;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/")
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
+}
