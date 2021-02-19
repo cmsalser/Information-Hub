12474f2a6cea61c6a7036af0ce7d83581f874728
DKrtl <dogukan_kartal98@hotmail.com>
2021-02-19 13:54:51 +0000

    Revert "setup user details service and repository with placeholder user class"
    This reverts commit 5a03ca5b23e3c1f9303c582686cfffbd5c3f8721.

--- src/main/java/com/project/informationhub/InformationhubApplication.java
+++ src/main/java/com/project/informationhub/InformationhubApplication.java
@@ -9,4 +9,5 @@ public class InformationhubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InformationhubApplication.class, args);
 	}
+
 }
--- src/main/java/com/project/informationhub/User.java
+++
@@ -1,4 +0,0 @@
-package com.project.informationhub;
-
-public class User {
-}
