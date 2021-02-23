9552c4cdab3b12ecf5f92f11b00b4b3dea156d49
RABBUL <rabbuluddowllah@gmail,com>
2021-02-23 19:56:20 +0600

    Added User Preferance to the system

---
+++ src/main/java/com/project/informationhub/UserPreferance/UserPreferanceRepository.java
@@ -0,0 +1,7 @@
+package com.project.informationhub.UserPreferance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface UserPreferanceRepository extends JpaRepository<UserPreferance, Long> {
+    
+}
