5392ace5fefd9c17fd5630a879511aa008119362
DKrtl <dogukan_kartal98@hotmail.com>
2021-02-24 21:04:30 +0000

    logout through /logout

--- src/main/java/com/project/informationhub/HomeResource.java
+++ src/main/java/com/project/informationhub/HomeResource.java
@@ -1,7 +1,13 @@
 package com.project.informationhub;
 
+import com.project.informationhub.User.CustomUserDetails;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class HomeResource {
@@ -9,10 +15,5 @@ public class HomeResource {
     @GetMapping("/")
     public String index() {
         return "<h1>Index</h1>";
-    }
-
-    @GetMapping("/user")
-    public String user() {
-        return ("<h1>Welcome User</h1>");
     }
 }
