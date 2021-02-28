277e7bea3d03006f8a1fd5a5489236d99d37aa72
edisemin <edisemin@hotmail.com>
2021-02-28 19:17:20 +0000

    Adding Thread and TopicForum classes

---
+++ src/main/java/com/project/informationhub/Thread.java
@@ -0,0 +1,66 @@
+package com.project.informationhub;
+
+import java.util.Date;
+/**
+ * 
+ * @author Class for storing information about threads
+ *
+ */
+public class Thread {
+	
+	private int accountID;
+	private int threadID;
+	private String title;
+	private String description;
+	private String newDesc;
+	private boolean anonymous;
+	private Date timestampCreated;
+	private Date timestampEdited;
+	private boolean stickied;
+	
+	public boolean createThread(int threadID,int accountID,String title,String description) {
+		return true;
+	
+	}
+	
+	public boolean createPost(String title,String description) {
+		return true;
+		
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void updatePost(String newDesc, String description) {
+		
+	}
+	
+	public void deletePost(int threadID) {
+		
+	}
+
+	public boolean getStickied() {
+		return stickied;
+	}
+
+	
+	public void setStickied(boolean stickied) {
+		this.stickied = stickied;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/project/informationhub/TopicForum.java
@@ -0,0 +1,15 @@
+package com.project.informationhub;
+
+/**
+ * Class for storing information about a TopicForum
+ *
+ */
+public class TopicForum {
+	private int forumID;
+	private String title;
+	
+	public String getTitle() {
+		return title;
+	}
+
+}
