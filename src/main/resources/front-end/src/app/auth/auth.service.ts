bc3eeb34015cfa4056a6b4bcd5c36a04a804c7ec
Salser, Charles (k1924604) <charlie@salser.com>
2021-03-06 17:12:57 +0000

    Add

--- src/main/resources/front-end/src/app/auth/auth.service.ts
+++ src/main/resources/front-end/src/app/auth/auth.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
+<<<<<<< Updated upstream
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+=======
+import { HttpClient } from '@angular/common/http';
+import { User } from "../models/user.model";
+>>>>>>> Stashed changes
 
 @Injectable({
   providedIn: 'root'
