7599d74b6895e679ca3f8c0bced0d9d429baf12a
Salser, Charles (k1924604) <charlie@salser.com>
2021-03-13 17:53:25 +0000

    Update forum.service.ts

--- src/main/resources/front-end/src/app/forum/forum.service.ts
+++ src/main/resources/front-end/src/app/forum/forum.service.ts
@@ -5,23 +5,28 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ForumService {
+  private postsURL: string;
+  private commentsURL: string;
 
-  constructor(private Http: HttpClient) { }
+  constructor(private Http: HttpClient) {
+    this.postsURL = 'https://jsonplaceholder.typicode.com/posts/';
+    this.commentsURL = 'https://jsonplaceholder.typicode.com/comments/';
+   }
 
   getThread(id) {
-    return this.Http.get('https://jsonplaceholder.typicode.com/posts/' + id);
+    return this.Http.get(this.postsURL + id);
   }
 
   getThreads() {
-    return this.Http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.Http.get(this.postsURL);
   }
 
   deleteThread(id) {
-    return this.Http.delete('https://jsonplaceholder.typicode.com/posts/' + id);
+    return this.Http.delete(this.postsURL + id);
   }
 
   editThread(body, id) {
-    return this.Http.patch('https://jsonplaceholder.typicode.com/posts/' + id, body);
+    return this.Http.patch(this.postsURL + id, body);
   }
 
   addThread(body, id) {
@@ -33,14 +38,14 @@ export class ForumService {
   }
   
   getPostById(id) {
-    return this.Http.get('https://jsonplaceholder.typicode.com/comments/' + id);
+    return this.Http.get(this.commentsURL + id);
   }
 
   deletePost(id) {
-    return this.Http.delete('https://jsonplaceholder.typicode.com/comments/' + id);
+    return this.Http.delete(this.commentsURL + id);
   }
 
   editPost(body, id) {
-    return this.Http.patch('https://jsonplaceholder.typicode.com/comments/' + id, body);
+    return this.Http.patch(this.commentsURL + id, body);
   }
 }
