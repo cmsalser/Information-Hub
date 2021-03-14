dd75fc13ae535569f29624c4440d20c55c0a305a
RABBUL <rabbuluddowllah@gmail,com>
2021-03-15 02:06:49 +0600

    merged master with main-page

--- src/main/java/com/project/informationhub/InformationhubApplication.java
+++ src/main/java/com/project/informationhub/InformationhubApplication.java
@@ -2,6 +2,7 @@ package com.project.informationhub;
 
 import com.project.informationhub.repository.ThreadRepository;
 import com.project.informationhub.repository.UserRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = {UserRepository.class, ThreadRepository.class})
 public class InformationhubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InformationhubApplication.class, args);
 	}
