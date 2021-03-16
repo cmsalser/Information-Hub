6c27f82e3c0274cb8234e2d2d19c99481004b481
ym1199 <ym74659@gmail.com>
2021-03-16 13:02:54 +0000

    Revert "Merge branch 'master' into main-page"
    This reverts commit f50995f2ccdac6d24f3588de6af304297e3c65e4, reversing
    changes made to 1ee5cff35dd3cf30b62c6d4d9d32253536f70a43.

--- src/main/front-end/src/app/app.module.ts
+++ src/main/front-end/src/app/app.module.ts
@@ -11,11 +11,7 @@ import { UserComponent } from './user/user.component';
 import {AuthService} from "./auth/auth.service";
 import { FaqEditComponent } from './faq/faq-edit/faq-edit.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { ForumComponent } from './forum/forum.component';
-import { ThreadComponent } from './forum/thread/thread.component';
-import { ThreadEditComponent } from './forum/thread-edit/thread-edit.component';
-import { PostEditComponent } from './forum/post-edit/post-edit.component';
-import { ThreadAddComponent } from './forum/thread-add/thread-add.component';
+
 
 @NgModule({
   declarations: [
@@ -26,12 +22,6 @@ import { ThreadAddComponent } from './forum/thread-add/thread-add.component';
     UserComponent,
     FaqEditComponent,
     SignUpComponent,
-    UserComponent,
-    ForumComponent,
-    ThreadComponent,
-    ThreadEditComponent,
-    PostEditComponent,
-    ThreadAddComponent
   ],
   imports: [
     BrowserModule,
