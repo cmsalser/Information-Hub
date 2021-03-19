045962083a1b5b4b24c49fe793b853d2146ad4a6
Yasemin Delen Mokhtari <ym74659@gmail.com>
2021-03-19 17:09:57 +0000

    Add and edit Sign up page functionality
    edit typescript for sign up
    add ReactiveFormsModule

--- src/main/front-end/src/app/sign-up/sign-up.component.ts
+++ src/main/front-end/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  //add option to input gender when "not listed" selected
+  gender: string[] = ['Female','Male', 'Transgender', 'Non-binary','Intersex', 'Not Listed: please provide details', 'Prefer not to say',''];
+  signUpForm: FormGroup;
+  constructor(private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.initializeSignUp();
   }
+  initializeSignUp(): void {
+    this.signUpForm = this.fb.group({
+      firstName: 'First Name',
+      LastName: 'Last Name',
+      username: 'user name',
+      password: 'password',
+      gender: '',
+      conditions: this.fb.group({
+        privacyPolicy: false,
+        termsAndConditions: false
+      }),
+      email: 'email here'
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.signUpForm);
+    
+  }
+
 
 }
