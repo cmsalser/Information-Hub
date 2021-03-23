2cecfdb4644f1b879053729f32e7d70157eea1d1
edisemin <edisemin@hotmail.com>
2021-03-23 14:04:39 +0000

    Fixed Post repository
    Fixed Post repository

--- src/main/java/com/project/informationhub/repository/PostRepository.java
+++ src/main/java/com/project/informationhub/repository/PostRepository.java
@@ -2,17 +2,21 @@ package com.project.informationhub.repository;
 
 import java.util.List;
 
-import com.project.informationhub.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.project.informationhub.model.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 	
 	List<Post> findByTitleContainingOrDescriptionContaining(String word, String word1);
 	
-	List<Post> findByThreadID(int threadId);
+	@Query("from Post p inner join fetch p.thread where p.thread.threadID = :threadId")
+	List<Post> findByThreadID(@Param("threadId")long threadId);
 	
-	List<Post> findByThreadIDAndStickied(long threadId, boolean sticked);
+	@Query("from Post p inner join fetch p.thread where p.thread.threadID = :threadId and p.thread.stickied=:stickied")
+	List<Post> findByThreadIDAndStickied(@Param("threadId")long threadId, @Param("stickied")boolean stickied);
 }
