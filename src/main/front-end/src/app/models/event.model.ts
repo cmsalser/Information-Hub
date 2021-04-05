65d506e82ca7bbdfadda7238df1f0a912f3047b6
ym1199 <ym74659@gmail.com>
2021-04-05 12:42:37 +0100

    Edit edit-event

--- src/main/front-end/src/app/models/event.model.ts
+++ src/main/front-end/src/app/models/event.model.ts
@@ -2,18 +2,20 @@ export class Event {
     userId: number;
     eventId: number;
     title: string;
-    startDate: Date = new Date();;
-    endDate: Date = new Date();;
+    startDate: Date;
+    endDate: Date;
     description: string;
-    body: string;
+    eventLink: string;
+    body: string; //don't need?
 
-    constructor(userId: number, eventId: number, title: string, startDate: Date, endDate: Date, description: string, body: string) {
+    constructor(userId: number, eventId: number, title: string, startDate: Date, endDate: Date, description: string, eventLink: string, body: string) {
         this.userId = userId;
         this.eventId = eventId;
         this.title = title;
         this.startDate = startDate;
         this.endDate = endDate;
         this.description = description;
-        this.body = body;
+        this.eventLink = eventLink;
+        this.body = body; //remove??, not used
     }
 }
